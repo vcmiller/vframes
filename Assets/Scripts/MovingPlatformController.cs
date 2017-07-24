6a658581ad1d7b1ca933cdb8c7c530059006d656
Vincent Miller <vmiller0725@gmail.com>
2017-07-23 21:33:43 -0700

    asdf

--- Assets/Scripts/MovingPlatformController.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class MovingPlatformController : MonoBehaviour {
-    public abstract Vector3 GetNextPos();
-}
