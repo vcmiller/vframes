2929de820039983b73457f9c4fca2b82ee5d3048
Vincent Miller <vmiller0725@gmail.com>
2017-07-26 19:44:04 -0700

    update bat file

--- GitGud.bat
+++ GitGud.bat
@@ -3,7 +3,15 @@ git add ProjectSettings/*
 git add .gitignore
 git add GitGud.bat
 
-set /p msg="Enter Commit Message: "
-git commit -m %msg%
+set /p msg="Commit Message: "
+git commit -m ^"%msg%^"
+
+set /p remote="Remote (blank for origin): "
+set /p branch="Branch (blank for master): "
+
+if %remote% == "" set remote="origin"
+if %branch% == "" set branch="master"
+
+git push ^"%remote%^" ^"%branch%^"
 
 pause
